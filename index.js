33153eae67fc6adac0bec2d18dc1209a3d69da03
Luke Metcalfe <luke.metcalfe@keyin.com>
2024-09-15 22:03:16 -0230

    Added the usage menu in case the user requires help. As well as added the code for the possible arguments that can be entered.

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const arguments = process.argv.slice(2);
 
 // Deals with the length flag 8 is default if length is not specified.
 const lengthArg = process.argv[3];
-const length = parseInt(lengthArg, 8) || 8;
+const length = parseInt(lengthArg, 10) || 8;
 
 // Customize argument
 const customizeArg = process.argv[4];
 
 // handle errors if no argument is eneterd
 function blankArg(){
-    console.error("An Argument must be entered... '--help' for more info");
+    console.log("An Argument must be entered... '--help' for more info");
 }
 
 // password generator 
@@ -86,3 +86,26 @@ function genPasswordWithEveything(value){
     return password;
 }
 
+// usage menu
+if(arguments.includes("--help")){
+    console.log(`
+        Usage:
+        --help  Displays the usage menu
+        --password  Generates a password with lower case characters
+        --password (length - should be an integer ex. 6) (choose what characters (a - all, c - capital, n - numbers, s - exsymbols ex. a)`)
+}
+
+// possible arguments
+if(arguments.length = 0){
+    blankArg();
+} else if (arguments.includes("--password") && customizeArg.includes("a")){
+    console.log("Password:" + genPasswordWithEveything(length))
+} else if (arguments.includes("--password") && customizeArg.includes("n")){
+    console.log("Password:" + genPasswordWithNums(length))
+} else if (arguments.includes("--password") && customizeArg.includes("c")){
+    console.log("Password" + genPasswordWithUppercase(length))
+} else if (arguments.includes("--password") && customizeArg.includes("s")){
+    console.log("Password:" + genPasswordWithSymbols(length))
+} else {
+    console.log("Password:" + genPassword(length))
+};
